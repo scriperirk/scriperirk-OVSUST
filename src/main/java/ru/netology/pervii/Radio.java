292d747328303bb81b44e76ff52324096b7c232b
Scriper.irk <scriper.irk@gmail.com>
2022-04-21 21:37:10 +0800

    next_3_0%

--- src/main/java/ru/netology/pervii/Radio.java
+++ src/main/java/ru/netology/pervii/Radio.java
@@ -2,70 +2,54 @@ package ru.netology.pervii;
 
 public class Radio {
 
-    private int radioVolume = 5;
-    int maxVolume = 10;
-    int minVolume = 0;
-    private int radioStation = 5;
-    int maxRadioStation = 9;
-    int minRadioStation = 0;
+    private int radioVolume;
 
-    //Станция
+    private int maxVolume = 100;
+    private int minVolume = 0;
+
+    private int radioStation;
+    private int maxRadioStation = 10;
+    private int minRadioStation = 0;
+
+    //конструкторы+
+    public Radio() {
+    }
+
+    public Radio(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation;
+    }
+    //-
+
+
+    public int getMaxRadioStation() {
+        return maxRadioStation;
+    }
+
     public int getRadioStation() {
-        return radioStation;
+        return this.radioStation;
     }
 
-    public void setRadioStation(int radioStation) {
-        this.radioStation = radioStation;
-    }
-
-    public int theStationByRemoteControl(int intendedStation) {
-        if (intendedStation <= 9 && intendedStation >= 0) {
-            radioStation = intendedStation;
+    public void setRadioStation(int forwardRadioStation) {
+        if (forwardRadioStation > maxRadioStation || forwardRadioStation < minRadioStation) {
+            return;
         }
-
-        return radioStation;
+        this.radioStation = forwardRadioStation;
     }
 
-
-    public void theStationByNextButton() {
+    public void nextRadioStation() {
         if (radioStation == maxRadioStation) {
-            radioStation = 0;
+            setRadioStation(minRadioStation);
         } else {
-            radioStation += 1;
+            setRadioStation(radioStation + 1);
         }
     }
 
-    public void theStationByPrevButton() {
+    public void prevRadioStation() {
         if (radioStation == minRadioStation) {
-            radioStation = 9;
+            setRadioStation(maxRadioStation);
         } else {
-            radioStation -= 1;
+            setRadioStation(radioStation - 1);
         }
     }
 
-    //Громкость
-    public int getRadioVolume() {
-        return radioVolume;
-    }
-
-    public void setRadioVolume(int radioVolume) {
-        this.radioVolume = radioVolume;
-    }
-
-    public void increaseVolume() {
-        if (radioVolume == maxVolume) {
-            return;
-        } else {
-            radioVolume += 1;
-        }
-    }
-
-    public void decreaseVolume() {
-        if (radioVolume == minVolume) {
-            return;
-        } else {
-            radioVolume -= 1;
-
-        }
-    }
 }
